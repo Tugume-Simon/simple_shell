f3c679e67c96730bd0870de0cfdfb4fefd0492da
Henry Joseph Phillip Lokuku <72735650+Hnryjsph@users.noreply.github.com>
2022-12-21 22:43:58 +0300

    Adding files from ubuntu version

---
+++ test.c
@@ -0,0 +1,17 @@
+
+#include <unistd.h>
+
+
+
+int main(int argc, char *argv[])
+{
+    char *str = "Hello world\n";
+
+    /* Possible warnings will be encountered here, about implicit declaration
+     * of `write` and `strlen`
+     */
+    write(1, str, strlen(str));
+    /* `1` is the standard output file descriptor, a.k.a. `STDOUT_FILENO` */
+
+    return 0;
+}
